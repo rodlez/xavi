0caa53828c5c5160786f6d38dc02215501a1de8b
Zug <zugzwangez@gmail.com>
2024-11-28 16:55:57 +0100

    Custom Section Component

--- resources/views/components/xavi/list.blade.php
+++ resources/views/components/xavi/list.blade.php
@@ -1,18 +1,20 @@
 @props([            
     'type' => 'ul',      // default value is 'ul'
-    'bgColor' => 'bg-red-400'
+    'bgColor' => '',
+    'titleList' => '',
 ])
 
 @php
     $options = [
-         'ul' => 'list-disc list-outside px-4',
-         'ol' => 'list-decimal list-outside px-4',
-         'group' => 'list-outside px-0 border border-gray-300 divide-y',
+         'ul' => 'list-disc list-outside px-8 py-2',
+         'ol' => 'list-decimal list-outside px-8 py-2',
+         'group' => 'list-outside p-4 border border-gray-300 divide-y',
      ];
-     $style = $options[$type] . ' ' . $bgColor ?? $options['ul']
+     // If there is no type defined default will be ul
+     $style = $options[$type] . ' ' . $bgColor ?? $options['ul'];     
 @endphp
 
-{{$style}}
+<p class="px-4 font-bold"> {{$titleList}} </p> 
 
 <ul {{ $attributes->merge(['class' => $style ]) }}>
     {{ $slot }}
--- resources/views/playground.blade.php
+++ resources/views/playground.blade.php
@@ -1,20 +1,46 @@
 <x-xavi-layout>
 
-<div>
-    <x-xavi.list title="Bullet list" type="ol" bgColor="bg-orange-400">
+    <x-slot name="title">Test</x-slot>
+
+    <x-xavi.list title="Order list" type="ol" bgColor="bg-orange-400" titleList="Albums">
         <li>Queen - <b>Greatest Hits</b></li>
         <li>The Rolling Stones - <i>Sticky Fingers</i></li>
         <li>The Beatles - Abbey Road</li>
         <li>The Who - Tommy</li>
     </x-xavi.list>
 
-    <x-xavi.list title="Bullet list">
+    <x-xavi.list title="Unorder list">
         <li>Queen - <b>Greatest Hits</b></li>
         <li>The Rolling Stones - <i>Sticky Fingers</i></li>
         <li>The Beatles - Abbey Road</li>
         <li>The Who - Tommy</li>
     </x-xavi.list>
 
-</div>
+    <x-xavi.list title="Group list" type="group" bgColor="bg-white">
+        <li>Queen - <b>Greatest Hits</b></li>
+        <li>The Rolling Stones - <i>Sticky Fingers</i></li>
+        <li>The Beatles - Abbey Road</li>
+        <li>The Who - Tommy</li>
+    </x-xavi.list>
+
+
+    <h2>Sections</h2>
+    <div class="grid grid-cols-3 gap-4">
+        <x-xavi.section class="col-span-3 md:col-span-1">
+            <h3>Section 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ducimus fuga nesciunt nisi quo sequi
+                voluptas. Accusantium consequuntur officiis veritatis.</p>
+        </x-xavi.section>
+        <x-xavi.section class="col-span-3 md:col-span-1">
+            <h3>Section 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab distinctio eos ex excepturi possimus,
+                reprehenderit vitae voluptatum. Accusamus eius eum ex, explicabo illo iste maxime odio soluta, vero
+                voluptas, voluptate!</p>
+        </x-xavi.section>
+        <x-xavi.section class="col-span-3 md:col-span-1">
+            <h3>Section 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, quasi?</p>
+        </x-xavi.section>
+    </div>
 
 </x-xavi-layout>
