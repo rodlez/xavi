69978ea3757150376f16a71a73c4cfcdf7b7d8e6
Zug <zugzwangez@gmail.com>
2024-12-12 13:34:44 +0100

    Function to get the translationsmissing Links

--- app/Livewire/Portfolio/Tags/PortfolioTags.php
+++ app/Livewire/Portfolio/Tags/PortfolioTags.php
@@ -2,7 +2,11 @@
 
 namespace App\Livewire\Portfolio\Tags;
 
+use App\Models\Languages;
 use App\Models\Portfolio\PortfolioTag;
+use App\Models\Portfolio\PortfolioTagTranslation;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -58,6 +62,86 @@ class PortfolioTags extends Component
         $this->sortLink = '<i class="fa-solid fa-caret-' . $caretOrder . '"></i>';
         $this->orderColumn = $columnName;
     }
+    
+
+    /**
+     * Given the element model, check for missing translations.
+     * 
+     * @return array With all the languages specifying which ones are missing.
+     */
+
+    public function translationLinks(model $element): array
+    {
+        // TODO: Refactor!!!
+        
+        //$langIds = $translationModel::where($idColumn, $IdValue)->pluck('lang_id')->toArray();
+        // 1 - Get the translations for the element
+        $elementTranslations = [];
+        foreach ($element->translations as $translation) {
+            //dd($translation);
+            $elementTranslations[] = $translation->language->id;
+        }
+
+        $totalTranslations = Languages::orderBy('id', 'asc')->pluck('id')->all();
+
+        $translationsMissing = array_diff($totalTranslations, $elementTranslations);
+
+        /* echo 'TOTAL<br/>';
+        var_dump($totalTranslations);
+        echo '<br/><br/>';
+        echo 'ELEMENT<br/>';
+        var_dump($elementTranslations);
+        echo '<br/><br/>';
+        echo 'translationsMissing<br/>';
+        var_dump($translationsMissing); */
+
+        // 1 - Get the translations for the element
+        $elementTranslations = [];
+        foreach ($element->translations as $translation) {
+            //dd($translation);
+            $elementTranslations[] = [
+                "elementId" => $translation->tag->id,
+                "translationId" => $translation->id,
+                "code" => $translation->language->code,
+                "langId" => $translation->language->id,
+                "translated" => true,
+            ];
+        }
+
+       /*  echo '<br/><br/>';
+        echo 'ELEMENT<br/>';
+        var_dump($elementTranslations); */
+
+        // 1 - Get the translations for the element
+        //Languages::orderBy('id', 'asc')->whereIn('id', $translationsMissing)->get();
+        $translationsPending = Languages::orderBy('id', 'asc')->whereIn('id', $translationsMissing)->get();
+        
+        $caca = array();
+        foreach ($translationsPending as $missing) {
+            //dd($missing);
+            $caca[] = [
+                "elementId" => $element->id,
+                "translationId" => null,
+                "code" => $missing->code,
+                "langId" => $missing->id,
+                "translated" => false,
+            ];
+        }
+        $total = array_merge($elementTranslations,$caca);
+       /*  echo '<br/><br/>';
+        echo 'translationsPending<br/>';
+        var_dump($caca);
+
+        $total = array_merge($elementTranslations,$caca);
+
+        echo '<br/><br/>';
+        echo 'TOTAL<br/>';
+        dd($total); */
+
+        return $total;
+    }
+
+
 
     public function render()
     {
@@ -86,5 +170,4 @@ class PortfolioTags extends Component
             'total' => $total,
         ])->layout('layouts.app');
     }
-    
 }
